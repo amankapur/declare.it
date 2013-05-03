64c2f44c0a47349d623504d2762421a5f1e740ea
Reyner Crosby <reyner@gmail.com>
2013-05-03 15:47:48 -0400

    filtering with hash

--- routes/scrapi.js
+++ routes/scrapi.js
@@ -54,24 +54,33 @@ exports.run = function(req,res) {
       'matsci': ['differntial equations', 'material science'],
       'systems': ['signals and systems', 'introduction to microelectronics circuits', 'software design', 'computer architecture', 'analog and digital communications', 'mechanics of solids and structures', 'dynamics', 'thermodynamics', 'transport phenomena', 'mechanical design', 'systems']
     }
-  
+  var relevant = filter_hash[conc];
 
   api('/files/transcript.html').get(function (err, json) {
     var distributions = ["AHSE", "ENGR", "MTH", "SCI", "OIE", "CC", "AWAY"];
-    var i, j, k, course, title, grade, credits;
+    var i, j, k, l, course, title, grade, credits, internal;
     var out = [];
-    for (j = 0; j < json.length; j++) {
-      for (k = 0; k< json[j].length; k++) {
-        if (json[j][k].indexOf(distributions[i]) == 0 && json[j][k].length > distributions[i].length) {
-          course = json[j][k];
-          title = json[j][k+1];
-          grade = json[j][k+2];
-          credits = json[j][k+4];
-          if (grade != "F" || grade != "NC") {
-            internal.push(course+ " "+title+"; "+credits+" credits of "+distributions[i]+".");
+    for (i = 0; i < distributions.length; i++) {
+      internal = [];
+      for (j = 0; j < json.length; j++) {
+        for (k = 0; k< json[j].length; k++) {
+          if (json[j][k].indexOf(distributions[i]) == 0 && json[j][k].length > distributions[i].length) {
+            course = json[j][k];
+            title = json[j][k+1];
+            grade = json[j][k+2];
+            credits = json[j][k+4];
+            console.log(title);
+            for (l = 0; l< relevant.length; l++){
+              if (title.toLowerCase().indexOf(relevant[l]) >= 0) {
+                if (grade != "F" || grade != "NC") {
+                  internal.push(course+ " "+title+"; "+credits+" credits of "+distributions[i]+".");
+                }
+              }
+            }
           }
         }
       }
+      out.push(internal);
     }
     res.json(out);
   })
