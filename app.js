c627f850be663d958d7feef51127a105264aa46f
Margaret-Ann Seger <margaret-ann@Margaret-Anns-MacBook-Air.local>
2013-07-24 21:24:15 +0200

    Added course req stuff

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.get('/concentrationReqs', loginRequired(), concentrationReqs.displayReqs);
 app.post('/login', scrapiroute.login);
 app.get('/logout', loginRequired(), scrapiroute.logout);
 
+app.get('/e-bio', loginRequired(), concentrationReqs.eBiology);
+/*app.get('/e-computing', loginRequired(), concentrationReqs.eComputing);
+app.get('/e-design', loginRequired(), concentrationReqs.eDesign);
+app.get('/e-matsci', loginRequired(), concentrationReqs.eMatsci);
+app.get('/e-systems', loginRequired(), concentrationReqs.eSystems);*/
+
 
 app.post('/autoFill', scrapiroute.run);
 app.post('/autoFillPlanInfo/:planID', planOfStudy.autoFillPlanInfo);
--- routes/concentration_reqs.js
+++ routes/concentration_reqs.js
@@ -6,3 +6,7 @@ var PlanOfStudy = require('../models/planOfStudy')
 exports.displayReqs = function(req, res){
 	res.render('concentration_reqs', {title: "Engineering Major Requirements"});
 }
+
+exports.eBiology = function(req, res){
+	res.render('reqs_eBio', {title: "Engineering: Bioengineering"});
+}
